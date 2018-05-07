f78e8410a58183604a7b5cc451232943ce33b737
dell-PC\dell <ghailan.abdelhamid@gmail.com>
2018-05-07 12:16:20 +0200

    2 commit

--- BOOT-1/src/main/java/com/example/boot/Boot1Application.java
+++ BOOT-1/src/main/java/com/example/boot/Boot1Application.java
@@ -3,7 +3,13 @@ package com.example.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication
+ * @author dell
+ *
+ */
 @SpringBootApplication
+
 public class Boot1Application {
 
 	public static void main(String[] args) {
